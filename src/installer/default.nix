bd0c66aac2706bb56db06fdd9e7b0fd6bd4f6e79
Emory Merryman <emory.merryman@gmail.com>
2018-10-17 18:17:13 -0400

    wtf

--- src/installer/default.nix
+++ src/installer/default.nix
@@ -1,13 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
 with import <nixpkgs> {};
-let script = pkgs.writeShellScriptBin "installer" ''
-  echo hello
-'';
-in
 stdenv.mkDerivation rec {
   name = "installer";
   src = ./src;
-  buildInputs = [ pkgs.gnupg ];
   makeFlags = [
     "DESTDIR=$out"
   ];
