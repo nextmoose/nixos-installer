21aeed7fbd6d18d7f24374d2a0aeb9aeece7ddaf
Emory Merryman <emory.merryman@gmail.com>
2018-10-18 15:16:05 -0400

    fix passphrase password snafu

--- src/installer/src/installer.sh
+++ src/installer/src/installer.sh
@@ -8,7 +8,7 @@ TEMP_DIR=$(mktemp -d) &&
 	    true
     } &&
     trap cleanup EXIT &&
-    read -s -p "SYMMETRIC PASSPHRASE? " SYMMETRIC_PASSWORD &&
+    read -s -p "SYMMETRIC PASSPHRASE? " SYMMETRIC_PASSPHRASE &&
     echo "${SYMMETRIC_PASSPHRASE}" | gpg --batch --passphrase-fd 0 --output ${TEMP_DIR}/secrets.tar.gz ${STORE_DIR}/etc/secrets.tar.gz.gpg &&
     gunzip --to-stdout ${TEMP_DIR}/secrets.tar.gz > ${TEMP_DIR}/secrets.tar &&
     tar --extract --file ${TEMP_DIR}/secrets.tar --directory ${TEMP_DIR}/secrets &&
