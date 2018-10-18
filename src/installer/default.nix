487c1b3f057bbe5adb26dce6b907237012bcd1ca
Emory Merryman <emory.merryman@gmail.com>
2018-10-18 11:38:47 -0400

    not quite

--- src/installer/default.nix
+++ src/installer/default.nix
@@ -1,4 +1,13 @@
 { pkgs ? import <nixpkgs> {} }:
+let
+  properties=pkgs.writeText "properties.txt" ''
+    GNUPG=${pkgs.gnupg}
+  '';
+  script=pkgs.writeShellScriptBin "xxx" ''
+    source properties.env
+    echo XXX
+  '';
+in
 with import <nixpkgs> {};
 stdenv.mkDerivation rec {
   name = "installer";
