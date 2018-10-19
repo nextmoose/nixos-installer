140a77830d0dca4504deaea608e28ea973ac442d
Emory Merryman <emory.merryman@gmail.com>
2018-10-19 11:26:29 -0400

    do it

--- src/installer/src/installed/init-read-only-pass/default.nix
+++ src/installer/src/installed/init-read-only-pass/default.nix
@@ -1,8 +1,12 @@
 { pkgs ? import <nixpkgs> {} }:
 with import <nixpkgs> {};
 let
-  precommit = pkgs.writeShellScriptBin "precommit" ''
+  precommit = pkgs.writeShellScriptBin "pre-commit" ''
     echo This is a read only repository.  No commits are allowed. &&
+      exit 64
+  '';
+  prepush = pkgs.writeShellScriptBin "pre-push" ''
+    echo This is a read only repository.  No pushes are allowed. &&
       exit 64
   '';
 in
@@ -14,9 +18,12 @@ stdenv.mkDerivation rec {
   '';
   installPhase = ''
     mkdir $out &&
+      mkdir $out/etc &&
+      cp secrets.tar.gz $out/etc &&
       mkdir $out/scripts &&
       cp init-read-only-pass.sh $out/scripts &&
-      makeWrapper $out/scripts/init-read-only-pass.sh $out/bin/init-read-only-pass --set PATH ${lib.makeBinPath [ mktemp coreutils gzip gnutar gnupg pass precommit ]} --set STORE_DIR $out &&
+      chmod 0500 $out/scripts/init-read-only-pass.sh &&
+      makeWrapper $out/scripts/init-read-only-pass.sh $out/bin/init-read-only-pass --set PATH ${lib.makeBinPath [ mktemp coreutils gzip gnutar gnupg pass precommit prepush which ]} --set STORE_DIR $out &&
       true
   '';
 }
--- src/installer/src/installed/init-read-only-pass/src/init-read-only-pass.sh
+++ src/installer/src/installed/init-read-only-pass/src/init-read-only-pass.sh
@@ -4,6 +4,36 @@ TEMP_DIR=$(mktemp -d) &&
     cleanup() {
 	rm --recursive --force ${TEMP_DIR} &&
     } &&
+    trap cleanup EXIT &&
+    while [ "${#}" -gt 0 ]
+    do
+	case "${1}" in
+	    --upstream-url)
+		UPSTREAM_URL="${2}" &&
+		    shift 2
+		;;
+	    --upstream-branch)
+		UPSTREAM_BRANCH="${2}" &&
+		    shift 2
+		;;
+	    *)
+		echo Unsupported Option &&
+		    echo ${1} &&
+		    echo ${0} &&
+		    echo ${@} &&
+		    exit 65
+		;;
+	esac
+    done &&
+    if [ -z "${UPSTREAM_URL}" ]
+    then
+	echo Undefined UPSTREAM_URL &&
+	    exit 66
+    elif [ -z "${UPSTREAM_BRANCH}" ]
+    then
+	echo Undefined UPSTREAM_URL &&
+	    exit 66
+    fi &&
     gunzip --to-stdout ${STORE_DIR}/etc/secrets.tar.gz > ${TEMP_DIR}/secrets.tar &&
     mkdir ${TEMP_DIR}/secrets &&
     tar --extract --file ${TEMP_DIR}/secrets.tar --directory ${TEMP_DIR}/secrets &&
@@ -14,7 +44,9 @@ TEMP_DIR=$(mktemp -d) &&
     pass init $(gpg --list-keys --with-colon | head --lines 5 | tail --lines 1 | cut --fields 5 --delimiter ":") &&
     pass git init &&
     pass git remote add upstream ${UPSTREAM_URL} &&
+    pass git remote set-url --push upstream no_push
     pass git remote fetch origin ${UPSTREAM_BRANCH} &&
     pass git checkout origin/${UPSTREAM_BRANCH} &&
-    ln --symbolic --force pre-commit ${HOME}/.password-store/.git/hooks &&
+    ln --symbolic --force $(which pre-commit) ${HOME}/.password-store/.git/hooks &&
+    ln --symbolic --force $(which pre-push) ${HOME}/.password-store/.git/hooks &&
     trust
