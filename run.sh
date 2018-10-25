b2dd6651e71f9bd74d5215e025ae96d9e373cf2d
Emory Merryman <emory.merryman@gmail.com>
2018-10-25 10:52:11 -0400

    status?

--- run.sh
+++ run.sh
@@ -5,11 +5,6 @@ STATUS=64 &&
     TEMP_DIR=$(mktemp -d) &&
     cleanup() {
 	rm --recursive --force ${TEMP_DIR} &&
-	    ((sudo VBoxManage controlvm nixos poweroff soft && sleep 10s) || true) && 
-	    (sudo VBoxManage unregistervm --delete nixos || true) &&
-	    (sudo rm ${DESTDIR}/nixos.vmdk || true) &&
-	    (sudo lvremove -f /dev/volumes/nixos || true) &&
-	    (rm --recursive --force ${DESTDIR} || true) &&
 	    exit ${STATUS} &&
 	    true
     } &&
@@ -99,19 +94,6 @@ EOF
 	    nix-${DESTDIR} '<nixpkgs/nixos>' -A config.system.${DESTDIR}.isoImage -I nixos-config=iso.nix &&
 	    true
     ) &&
-    sudo lvcreate -y --name nixos --size 100G volumes &&
-    sudo VBoxManage internalcommands createrawvmdk -filename ${DESTDIR}/nixos.vmdk -rawdisk /dev/volumes/nixos &&
-    sudo VBoxManage createvm --name nixos --register &&
-    sudo VBoxManage storagectl nixos --name "SATA Controller" --add SATA &&
-    sudo VBoxManage storageattach nixos --storagectl "SATA Controller" --port 0 --device 0 --type dvddrive --medium build/installation/result/iso/nixos-18.03.133245.d16a7abceb7-x86_64-linux.iso &&
-    sudo VBoxManage storagectl nixos --name "IDE" --add IDE &&
-    sudo VBoxManage storageattach nixos --storagectl "IDE" --port 0 --device 0 --type hdd --medium ${DESTDIR}/nixos.vmdk &&
-    sudo VBoxManage modifyvm nixos --memory 2000 &&
-    sudo VBoxManage modifyvm nixos --nic1 nat &&
-    sudo VBoxManage modifyvm nixos --firmware efi &&
-    sudo VBoxManage startvm nixos &&
-    read -p "IS IT OK? y/n " ISITOK &&
-    [ "${ISITOK}" == "y" ] &&
     STATUS=0 &&
     cp build/installation/result/iso/nixos-18.03.133245.d16a7abceb7-x86_64-linux.iso . &&
     true
